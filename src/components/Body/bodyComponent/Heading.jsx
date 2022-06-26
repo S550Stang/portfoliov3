d14c4e76d3cf1088700f277ef8497e628ee7c271
S550Stang <prabhuvardhanrao94@gmail.com>
2022-06-26 09:11:28 -0400

    adjust the padding for the Projects button

--- src/components/Body/bodyComponent/Heading.jsx
+++ src/components/Body/bodyComponent/Heading.jsx
@@ -12,7 +12,7 @@ export const Heading = () => {
       <br />
       <span className="text-[#3F37C9] underline">Web Dev.</span>
       <br />
-      <button className="bg-[#3F37C9] text-3xl p-2 rounded-3xl cursor-pointer ">
+      <button className="bg-[#3F37C9] text-3xl px-4 py-2 rounded-3xl cursor-pointer ">
         <a href="#MyWork">Projects.</a>
       </button>
     </div>
