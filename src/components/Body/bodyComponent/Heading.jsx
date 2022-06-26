7c9fc85177812a0d75825c723e345d9cd18067d3
S550Stang <prabhuvardhanrao94@gmail.com>
2022-06-26 09:44:18 -0400

    heading refactor with svg

--- src/components/Body/bodyComponent/Heading.jsx
+++ src/components/Body/bodyComponent/Heading.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "../../../assets/image.svg";
 
 export const Heading = () => {
   return (
     <div
       id="Home"
-      className="container mx-auto text-5xl break-words p-5 space-y-4"
+      className="container mx-auto font-bold text-5xl sm:text-6xl md:text-7xl lg:text:8xl  break-words p-5 space-y-4"
     >
       Hi, i'm Prabhu.
       <br />
@@ -12,9 +13,12 @@ export const Heading = () => {
       <br />
       <span className="text-[#3F37C9] underline">Web Dev.</span>
       <br />
-      <button className="bg-[#3F37C9] text-3xl px-4 py-2 rounded-3xl cursor-pointer ">
-        <a href="#MyWork">Projects.</a>
-      </button>
+      <div className="flex justify-between items-center">
+        <button className="bg-[#3F37C9] font-light text-3xl px-4 py-2 rounded-3xl cursor-pointer ">
+          <a href="#MyWork">Projects.</a>
+        </button>
+        <img src={Image} atl="image" className="hidden md:block w-40" />
+      </div>
     </div>
   );
 };
