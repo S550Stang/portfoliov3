9492642b4f8d4d069cd0a9718d16284855d0a3a0
S550Stang <prabhuvardhanrao94@gmail.com>
2022-06-25 20:54:36 -0400

    refactor react boiler plate code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { FullPage } from "./components/FullPage";
+function App() {
+  return (
+    <div>
+      <FullPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FullPage.js
@@ -0,0 +1,13 @@
+import { Navbar } from "./Navbar/Navbar";
+import { Body } from "./Body/Body";
+import { Footer } from "./Footer/Footer";
+
+export const FullPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
